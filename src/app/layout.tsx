23d4c4dec68d4a7892b7b0f90d8c1a20baef1eef
rudinesurya <rudinesurya@gmail.com>
2025-05-25 19:08:28 +0100

    updated with shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavigationBar from "./NavigationBar";
+import { ThemeProvider } from 'next-themes';
+import { Toaster } from 'sonner'; // Add this import
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider attribute='class'>
+          <NavigationBar />
+          {children}
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
